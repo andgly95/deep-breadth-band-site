39dfb0c5eaa2836e835874cb7a5cb0e08ffc6f7b
Andrew Glyadchenko <andgly95@users.noreply.github.com>
2023-09-18 14:49:02 -0400

    init: project, theme, nav

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+  <title>About</title>
+  <meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+  <h1>About Deep Breadth</h1>
+
+  <p>
+    The page you're looking at is purely static HTML, with no client-side
+    interactivity needed. Because of that, we don't need to load any JavaScript.
+    Try viewing the page's source, or opening the devtools network panel and
+    reloading.
+  </p>
+</div>
